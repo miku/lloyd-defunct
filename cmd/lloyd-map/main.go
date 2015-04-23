4439c9d26e957e77d0d09328e32fcbdb922b59f1
Martin Czygan <martin.czygan@gmail.com>
2015-04-23 09:16:31 +0200

    add support for multiple keys

--- cmd/lloyd-map/main.go
+++ cmd/lloyd-map/main.go
@@ -12,19 +12,21 @@ import (
 	"log"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync"
 )
 
 // Batch contains lines and the offset from which they originate.
 type Batch struct {
-	Key        string
+	Keys       []string
 	Lines      []string
 	BaseOffset int64
 }
 
 // RecordInfo contains the value and the document offset and length within the file.
 type RecordInfo struct {
-	Value  interface{}
+	Values []string
 	Offset int64
 	Length int64
 }
@@ -37,12 +39,27 @@ func worker(batches chan Batch, out chan RecordInfo, wg *sync.WaitGroup) {
 		for _, line := range batch.Lines {
 			dst := make(map[string]interface{})
 			json.Unmarshal([]byte(line), &dst)
-			value, ok := dst[batch.Key]
-			if !ok {
-				log.Fatal("missing key")
+
+			var values []string
+			for _, key := range batch.Keys {
+				value, ok := dst[key]
+				if !ok {
+					log.Fatal("missing key")
+				}
+				switch value.(type) {
+				case string:
+					values = append(values, value.(string))
+				case int:
+					values = append(values, fmt.Sprintf("%d", value.(int)))
+				case float64:
+					values = append(values, strconv.FormatFloat(value.(float64), 'f', 6, 64))
+				default:
+					log.Fatal("value type mismatch")
+				}
 			}
+
 			length := int64(len(line))
-			ri := RecordInfo{Value: value, Offset: offset, Length: length}
+			ri := RecordInfo{Values: values, Offset: offset, Length: length}
 			out <- ri
 			offset += length
 		}
@@ -52,14 +69,22 @@ func worker(batches chan Batch, out chan RecordInfo, wg *sync.WaitGroup) {
 func sink(w io.Writer, out chan RecordInfo, done chan bool) {
 	bw := bufio.NewWriter(w)
 	for ri := range out {
-		bw.WriteString(fmt.Sprintf("%s\t%d\t%d\n", ri.Value, ri.Offset, ri.Length))
+		bw.WriteString(fmt.Sprintf("%s\t%d\t%d\n", strings.Join(ri.Values, "\t"), ri.Offset, ri.Length))
 	}
 	bw.Flush()
 	done <- true
 }
 
+func parseList(s string) (items []string) {
+	fields := strings.Split(s, ",")
+	for _, f := range fields {
+		items = append(items, strings.TrimSpace(f))
+	}
+	return
+}
+
 func main() {
-	key := flag.String("key", "", "key to extract (top-level only)")
+	rawKeys := flag.String("keys", "", "key or keys to extract (top-level only)")
 	workers := flag.Int("w", runtime.NumCPU(), "number of workers")
 	size := flag.Int("size", 10000, "number of lines in one batch")
 
@@ -94,27 +119,33 @@ func main() {
 	var lines []string
 	var baseOffset, offset int64
 
+	keys := parseList(*rawKeys)
+
 	for {
 		s, err := reader.ReadString('\n')
+
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		lines = append(lines, s)
 		offset += int64(len(s))
+
 		if len(lines)%*size == 0 {
 			l := make([]string, len(lines))
 			copy(l, lines)
-			batches <- Batch{Lines: l, Key: *key, BaseOffset: baseOffset}
+			batches <- Batch{Lines: l, Keys: keys, BaseOffset: baseOffset}
 			lines = lines[:0]
 			baseOffset = offset
 		}
 	}
+
 	l := make([]string, len(lines))
 	copy(l, lines)
-	batches <- Batch{Lines: l, Key: *key, BaseOffset: baseOffset}
+	batches <- Batch{Lines: l, Keys: keys, BaseOffset: baseOffset}
 	lines = lines[:0]
 
 	close(batches)
