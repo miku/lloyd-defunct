a13ecf682aa50e481e55a9af4ef9feae1de3d4d3
Martin Czygan <martin.czygan@gmail.com>
2015-04-27 13:54:14 +0200

    wip: mmap

--- cmd/lloyd-permute/main.go
+++ cmd/lloyd-permute/main.go
@@ -1,5 +1,15 @@
 // lloyd-permute takes a list of offset, length pairs from Stdin
 // and outputs the parts in the order as they are read.
+//
+// w/ mmap
+//
+// * read lines from (offset, length) until a fixed offset (e.g. 100M, 1G)
+// * mmap region for reading (R)
+// * mmap a file for writing (W)
+// * copy over slices from R to W
+// * flush W and append to previously written parts
+// * repeat until done
+
 package main
 
 import (
@@ -12,8 +22,51 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/edsrzf/mmap-go"
 	"github.com/miku/lloyd"
 )
+
+type SeekInfo struct {
+	Offset int64
+	Length int64
+}
+
+func lower(val, size int64) int64 {
+	return val + (size - val%size) - size
+}
+
+func process(sis []SeekInfo, filename string) {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	pagesize := os.Getpagesize()
+
+	if len(sis) > 0 {
+		first := sis[0]
+		last := sis[len(sis)-1]
+		log.Println(len(sis), "items", first.Offset, last.Offset+last.Length)
+
+		regionOffset := lower(first.Offset, int64(pagesize))
+
+		diff := regionOffset - first.Offset
+		log.Println(diff)
+
+		regionLength := int(first.Offset - regionOffset + last.Offset + last.Length)
+		log.Println("mmap region", regionOffset, regionLength)
+		mm, err := mmap.MapRegion(file, regionLength, mmap.RDONLY, 0, regionOffset)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(mm[first.Offset-diff : first.Length-diff]))
+	} else {
+		log.Println("empty")
+	}
+}
 
 func main() {
 	version := flag.Bool("v", false, "prints current program version")
@@ -29,13 +82,18 @@ func main() {
 		log.Fatal("input file required")
 	}
 
-	file, err := os.Open(flag.Arg(0))
+	filename := flag.Arg(0)
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	reader := bufio.NewReader(os.Stdin)
+
+	var slices []SeekInfo
+	windowSize := 1000000
+	currentWindow := windowSize
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -57,12 +115,24 @@ func main() {
 		if err != nil {
 			log.Fatalf("invalid length: %s", fields[1])
 		}
-		buf := make([]byte, length)
-		file.Seek(int64(offset), 0)
-		_, err = file.Read(buf)
-		if err != io.EOF && err != nil {
-			log.Fatal(err)
+
+		si := SeekInfo{Offset: int64(offset), Length: int64(length)}
+		if offset > currentWindow {
+			// hand over to mmap
+			currentWindow = currentWindow + windowSize
+			dst := make([]SeekInfo, len(slices))
+			copy(dst, slices)
+			process(dst, filename)
+			slices = slices[:0]
 		}
-		fmt.Printf(string(buf))
+		slices = append(slices, si)
+
+		// buf := make([]byte, length)
+		// file.Seek(int64(offset), 0)
+		// _, err = file.Read(buf)
+		// if err != io.EOF && err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Printf(string(buf))
 	}
 }
