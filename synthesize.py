0fe024e99138d61396eb78e6bd1fd919deb0abcd
Martin Czygan <martin.czygan@gmail.com>
2015-06-09 17:22:17 +0200

    synthesize larger files

--- synthesize.py
+++ synthesize.py
@@ -2,5 +2,5 @@
 
 import json, random
 
-for i in range(10000000):
-    print(json.dumps({"id": random.randint(0, 10000), "value": i}))
+for i in range(100000000):
+    print(json.dumps({"id": random.randint(0, 100000), "value": i}))
